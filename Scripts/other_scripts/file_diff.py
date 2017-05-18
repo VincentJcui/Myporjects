663488db76b3f070db22cdca66de6857a2099ce0
VincentJcui <cui6522123@gmail.com>
2017-05-18 11:21:06 +0800

    文件比对不同

--- Scripts/other_scripts/file_diff.py
+++ Scripts/other_scripts/file_diff.py
@@ -1,19 +1,20 @@
 #encoding:utf-8
 import sys
 import difflib
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 file1 = sys.argv[1]
 file2 = sys.argv[2]
 
-
 with open(file1,'r') as f:
     file1_list = f.readlines()  #按行读取全文
 
-with open(file1,'r') as f:
+with open(file2,'r') as f:
     file2_list = f.readlines()
 
 
-#方法一,形成html对比结果
+#方法一,形成html对比结果,涉及到中文对比,在生产的html中指定了页面的编码,需要手动修改,或者在源码中修改
 diff = difflib.HtmlDiff()
 html = diff.make_file(file1_list, file2_list)
 with open('diff.html','w') as f:
