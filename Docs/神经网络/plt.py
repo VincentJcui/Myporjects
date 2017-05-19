fb652ca828105350c153f080c226980cf8ba5b6a
VincentJcui <cui6522123@gmail.com>
2017-05-19 17:43:18 +0800

    解决后端分页当分页数量较多时,最后一页为空页的bug

--- Docs/神经网络/plt.py
+++ Docs/神经网络/plt.py
@@ -13,7 +13,7 @@ X = df.iloc[0:100 ,[0, 2]].values
 
 plt.scatter(X[:50, 0], X[:50, 1], color='red', marker='o', label='setosa')
 plt.scatter(X[50:100, 0], X[50:100, 1], color='blue', marker='x', label='versicolor')
-plt.xlabel('花瓣长度')
-plt.ylabel('花茎长度')
+plt.xlabel('length_X')
+plt.ylabel('length_Y')
 plt.legend(loc='upper left')
 plt.show()
