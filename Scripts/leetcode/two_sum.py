f9f3a5a1115eec4aeb8bcef62f2fe6a431797259
VincentJcui <cui6522123@gmail.com>
2017-06-22 12:04:48 +0800

    Revert: 增加收入明细,区分平台收入和官网收入

---
+++ Scripts/leetcode/two_sum.py
@@ -0,0 +1,30 @@
+#encoding:utf-8
+
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice
+
+Example:
+Given nums = [2, 7, 11, 15], target = 9,
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+'''
+
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for x in range(len(nums)):
+            for y in range(x+1,len(nums)):
+                if nums[x] + nums[y] == target:
+                    return [x, y]
+
+nums = [2, 3, 4, 6, 7]
+target = 13
+n = Solution()
+print n.twoSum(nums, target)
